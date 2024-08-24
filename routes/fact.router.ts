da71c2980a96b243d058732f8989054af99e5c4c
Chad Smith <chad.e.smith@outlook.com>
2024-08-23 20:13:26 -0500

    REFACTOR: router on server to return object instead of string.

--- routes/fact.router.ts
+++ routes/fact.router.ts
@@ -4,7 +4,7 @@ const factRouter = Router();
 
 factRouter.get('/', async (req: Request, res: Response) => {
     console.log('Fact requested!');
-    const fact = 'I craft gluten-free postcards.';
+    const fact = { message: 'I craft gluten-free postcards.' };
     res.send(fact).status(200);
 });
 
