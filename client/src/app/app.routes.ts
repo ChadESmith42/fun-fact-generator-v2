65d9a7ccb7ea991eab6292feb19dc38c4844c62f
Chad Smith <chad.e.smith@outlook.com>
2024-08-25 15:36:11 -0500

    FIX: add path for popular facts

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,4 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: 'fact', loadComponent: () => import('../components/fact/fact.component').then(c => c.FactComponent) },
+  { path: 'popular', loadComponent: () => import('../components/popular/popular.component').then(c => c.PopularComponent) },
 ];
