b75bbca794e267ddd93e576b03d622ec65d8fccf
Chad Smith <chad.e.smith@outlook.com>
2024-08-27 20:05:31 -0500

    FIX: unit testing

--- client/src/services/fact.service.spec.ts
+++ client/src/services/fact.service.spec.ts
@@ -1,12 +1,18 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClient } from '@angular/common/http';
 import { FactService } from './fact.service';
 
 describe('FactService', () => {
   let service: FactService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useValue: HttpClientTestingModule},
+      ]
+    });
     service = TestBed.inject(FactService);
   });
 
